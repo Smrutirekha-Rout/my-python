5933d9cd0951292ee7b1a8fe0ee65a59a5cbaafe
Smrutirekha-Rout <163122913+Smrutirekha-Rout@users.noreply.github.com>
2024-03-12 15:42:41 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,37 @@
+from operator import truediv
+
+
+a=20
+b=30
+c=a+b
+print(c)
+a=50+60j
+print(type(a))
+
+str="welcome to nalanda"
+print(str)
+
+msg='welcome to CSE'
+print(msg)
+
+msg1='''welcome to python'''
+print(msg1)
+
+msg2=""""welcome to CSE department"""
+print(msg2)
+
+address='''at_haripur
+           from_BHADRAK
+           pin_756131'''
+print(address)
+
+a=True
+print(type(a))
+
+b=50
+c=30
+d=b<c
+print(d)
+
+str="welcome to cse"
+print(str[1:7:1])
